d4668f53d368f889a4d43059282be411ac333c5e
mirzace <mirzace@maestralsolutions.com>
2021-08-30 16:07:29 +0200

    Initial BE Setup

---
+++ Entities/Actor.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace api.Entities
+{
+    public class Actor
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public ICollection<Screenplay> Screenplays { get; set; }
+
+    }
+}
---
+++ Entities/Rating.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace api.Entities
+{
+    public class Rating
+    {
+        public int Id { get; set; }
+        public int Rate { get; set; }
+        [Required]
+        public Screenplay Screenplay { get; set; }
+    }
+}
